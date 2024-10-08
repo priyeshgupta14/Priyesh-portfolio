ee1cb41f35115305cedc901b846078e32b7484a5
priyeshgupta14 <21bds050@iiitdwd.ac.in>
2024-10-08 21:54:59 +0530

    experience page changes

--- src/layouts/Home/Experience.js
+++ src/layouts/Home/Experience.js
@@ -5,8 +5,8 @@ import NITPY from "layouts/Home/NITPY";
 import styles from './Experience.module.css'; // Import the CSS module
 
 export const Experience = () => {
-  const barRef = React.useRef(null);
-  const [DescriptionJob, setDescriptionJob] = React.useState("Advanced Agro Management");
+  // Set IIITB as the default experience section
+  const [DescriptionJob, setDescriptionJob] = React.useState("IIITB");
 
   const GetDescription = () => {
     switch (DescriptionJob) {
@@ -23,38 +23,41 @@ export const Experience = () => {
     <div data-aos="fade-up" className={styles.container}>
       <section className={styles.titleSection}>
         <div className={styles.titleSection}>
-          <span className={styles.titleNumber}> 02.</span>
+          <span className={styles.titleNumber}></span>
         </div>
         <span className={styles.titleText}>Where I&apos;ve Worked</span>
         <div className={styles.divider}></div>
       </section>
       <section className={styles.experienceSection}>
         <CompaniesBar setDescriptionJob={setDescriptionJob} />
-        {GetDescription()}
+        <ul className={styles.experienceList}>
+          <li>{GetDescription()}</li> {/* Bullet point for each experience */}
+        </ul>
       </section>
     </div>
   );
 };
 
 const CompaniesBar = (props) => {
-  const [barPosition, setBarPosition] = React.useState(-8);
-  const [barAbovePosition, setBarAbovePosition] = React.useState(0);
-  const [companyNameBackgroundColorGreen, setCompanyNameBackgroundColorGreen] = React.useState([true,false]);
+  // Set initial positions for the bar and selection
+  const [barPosition, setBarPosition] = React.useState(-10);
+  const [barAbovePosition, setBarAbovePosition] = React.useState(1);
+  const [companyNameBackgroundColorGreen, setCompanyNameBackgroundColorGreen] = React.useState([true, false]);
 
   const CompanyButton = (props) => (
-    <button
-      onClick={() => {
-        setBarPosition(props.BarPosition);
-        setBarAbovePosition(props.BarAvobePosition);
-        props.setDescriptionJob(props.DescriptionJob);
-        setCompanyNameBackgroundColorGreen(props.CompanyNameBackgroundColorGreen);
-      }}
-      className={`${styles.companyButton} ${
-        companyNameBackgroundColorGreen[props.ButtonOrderOfcompanyNameBackgroundColorGreen] && styles.selectedCompanyButton
-      }`}
-    >
-      {props.CompanyName}
-    </button>
+<button
+    onClick={() => {
+      setBarPosition(props.BarPosition);
+      setBarAbovePosition(props.BarAvobePosition);
+      props.setDescriptionJob(props.DescriptionJob);
+      setCompanyNameBackgroundColorGreen(props.CompanyNameBackgroundColorGreen);
+    }}
+    className={`${styles.companyButton} ${
+      companyNameBackgroundColorGreen[props.ButtonOrderOfcompanyNameBackgroundColorGreen] && styles.selectedCompanyButton
+    }`}
+  >
+    {props.CompanyName}
+  </button>
   );
 
   return (
@@ -63,22 +66,22 @@ const CompaniesBar = (props) => {
         <motion.div animate={{ y: barPosition }} className={styles.animatedBar}></motion.div>
       </div>
       <div className={`${styles.flexCol} ${styles.spaceY1} ${styles.paddingLeft8} ${styles.paddingLeft0Md}`}>
-      <CompanyButton
+        <CompanyButton
           ButtonOrderOfcompanyNameBackgroundColorGreen={0}
           CompanyName="IIITB"
           BarPosition={-10}
           BarAvobePosition={1}
           DescriptionJob="IIITB"
-          CompanyNameBackgroundColorGreen={[true,false]}
+          CompanyNameBackgroundColorGreen={[true,false]} // IIITB selected by default
           setDescriptionJob={props.setDescriptionJob}
         />
         <CompanyButton
-          ButtonOrderOfcompanyNameBackgroundColorGreen={0}
+          ButtonOrderOfcompanyNameBackgroundColorGreen={1} // NITPY not selected
           CompanyName="NITPY"
-          BarPosition={-10}
-          BarAvobePosition={1}
+          BarPosition={40}
+          BarAvobePosition={2}
           DescriptionJob="NITPY"
-          CompanyNameBackgroundColorGreen={[false,true]}
+          CompanyNameBackgroundColorGreen={[false,true]} // NITPY unselected
           setDescriptionJob={props.setDescriptionJob}
         />
       </div>
